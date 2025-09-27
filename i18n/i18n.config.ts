7582caf63ae1cf5e886a774f628a1f9c8a2b35a4
Mohamed Ibrahem <65599462+mo7mad1996@users.noreply.github.com>
2025-09-27 19:05:05 +0300

    Fix i18n spread keys and create user settings page

--- i18n/i18n.config.ts
+++ i18n/i18n.config.ts
@@ -1,21 +1,12 @@
 export default defineI18nConfig(() => ({
-	// modules
-	// i18n: {
-	// 	locales: [
-	// 		{ code: "ar", file: "ar.json", dir: "rtl", name: "العربية" },
-	// 		{ code: "en", file: "en.json", dir: "ltr", name: "English" },
-	// 	],
-	// 	strategy: "no_prefix",
-	// 	autoDeclare: true,
-	// 	langDir: "lang",
-	// 	defaultLocale: "ar",
-
 	legacy: false,
 	messages: {
 		ar: {
+			...require("./lang/ar.json"),
 			nav: require("./lang/nav/ar.json"),
 		},
 		en: {
+			...require("./lang/en.json"),
 			nav: require("./lang/nav/en.json"),
 		},
 	},
