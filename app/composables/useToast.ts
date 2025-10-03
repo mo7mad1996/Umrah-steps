3ca779f408309aed22eb2e005817b13669a1e4f5
Mohamed Ibrahem <65599462+mo7mad1996@users.noreply.github.com>
2025-10-03 22:03:03 +0300

    Updated useUser.ts

--- app/composables/useToast.ts
+++ app/composables/useToast.ts
@@ -2,24 +2,25 @@ import { updateGlobalOptions } from "vue3-toastify";
 
 export const useToast = function () {
 	const { $toast } = useNuxtApp();
+	const { locale } = useI18n();
 
-	// const { locale } = useI18n();
+	if (process.client) {
+		const isDark = document.documentElement.classList.contains("dark");
 
-	updateGlobalOptions({
-		position: $toast.POSITION.BOTTOM_CENTER,
-
-		transition: "bounce",
-		closeButton: false,
-		progressClassName: "!p-3 ",
-		bodyClassName: "!text-black dark:!text-white font-bold",
-		hideProgressBar: true,
-		toastClassName: "dark:!bg-black/90 !bg-white/90 rounded-lg shadow-lg",
-		containerClassName: "!p-1",
-		autoClose: 2600,
-
-		// theme: theme.name.value as "dark" | "light",
-		// rtl: locale.value === "ar",
-	});
+		updateGlobalOptions({
+			position: $toast.POSITION.BOTTOM_CENTER,
+			transition: "bounce",
+			closeButton: false,
+			progressClassName: "!p-3",
+			bodyClassName: "!text-black dark:!text-white font-bold",
+			hideProgressBar: true,
+			toastClassName: "dark:!bg-black/90 !bg-white/90 rounded-lg shadow-lg",
+			containerClassName: "!p-1",
+			autoClose: 2600,
+			theme: isDark ? "dark" : "light",
+			rtl: locale.value === "ar",
+		});
+	}
 
 	return $toast;
 };
--- app/composables/useUser.ts
+++ app/composables/useUser.ts
@@ -3,15 +3,22 @@ import type { User_DB_Schema } from "~/types/user";
 export const useUser = () => {
 	const { t } = useI18n();
 
-	const { data, error, status, refresh } = useAsyncData(`me`, () =>
-		useApi()
-			.get("/me")
-			.then((r) => r.data as User_DB_Schema),
+	const { data, error, status, refresh } = useAsyncData(
+		`me`,
+		() =>
+			useApi()
+				.get("/me")
+				.then((r) => r.data as User_DB_Schema),
+		{
+			server: false,
+		},
 	);
 
 	watch(error, (val) => {
-		if (val) {
+		if (val && process.client) {
 			console.error(val);
+			const token = useCookie("token");
+			token.value = undefined;
 			navigateTo("/login");
 			useToast().error(t("dashboard.forceLogout"));
 		}
